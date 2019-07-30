4de0132f8ffd70818f907baf68592b840a708379
myname21-meet <myname21@meet.mit.edu>
2019-07-30 05:14:30 -0400

    forms

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from databases import *
 from flask import Flask, render_template, url_for, request
 app = Flask(__name__)
 
+@app.route('/add', methods=['GET','POST'])
+def add():
+	return render_template('add.html', add_student_route())
+
+
 @app.route('/')
 def home():
     return render_template('home.html', students=query_all())
